b8ca91dbfb69637cef4ec33e23574810796ecd4d
ChenYu <cgeng.charlie@gmail.com>
2024-02-06 23:23:44 -0600

    Add Authentication with Clerk

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: 'SmartNote - Notes',
+}
+
+const page = () => {
+	return <div>This is a note page</div>
+}
+
+export default page
